9a3e58bf712d000af67147b41e1861f7ae4b9a5e
Matías Iturburu <matias.iturburu@tallertechnologies.com>
2012-09-04 16:25:09 -0300

    Modelos más simples, tareas más completas

--- django_celery/picproject/pictures/models.py
+++ django_celery/picproject/pictures/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 import tasks
+
     
 class Album(models.Model):
 
     name = models.CharField(max_length=100)
-    url = models.URLField(max_length=200)
+    set_id = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        tasks.process_pictures(self)
-        return super(Album, self).save(self, *args, **kwargs)
-    
 class Picture(models.Model):
 
-    image = models.ImageField(upload_to='images')
+    image_url = models.URLField()
     album = models.ForeignKey('pictures.Album')
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    map_url = models.URLField()
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    map_url = models.URLField(blank=True, null=True)
+    
+
+@receiver(post_save, sender=Album)
+def start_task(sender, **kwargs):
+    tasks.process_pictures(kwargs['instance'])
--- django_celery/picproject/pictures/tasks.py
+++ django_celery/picproject/pictures/tasks.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import models
 import json
 
+
 flickr = FlickrAPI(settings.FLICKR_KEY,
                settings.FLICKR_SECRET)
 
@@ -12,14 +13,17 @@ gm = GoogleMaps(settings.GOOGLE_KEY)
 
 @task
 def process_pictures(album):
-        
-    for x in flickr.walk_set(album.url.split('/')[-2]):
-        pic_url = 'http://farm%s.staticflickr.com/%s/%s-%s_z.jpg' %(
-            x.get('farm'), 
-            x.get('server'), 
-            x.get('id'), 
+    for x in flickr.walk_set(album.set_id):
+        pic_url = 'http://farm%s.staticflickr.com/%s/%s_%s_z.jpg' %(
+            x.get('farm'),
+            x.get('server'),
+            x.get('id'),
             x.get('secret')
         )
-        
-        
-        
+        location = flickr.photos_geo_getLocation(x.get('id'))
+        import ipdb; ipdb.set_trace()
+        pic = models.Picture(
+            album=album,
+            image_url=pic_url
+        )
+        pic.save()
